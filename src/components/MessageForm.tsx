d2d7e9838356892435363c9368eacd76f6609f57
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-08-05 18:53:16 +0530

    complet ui part

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Textarea } from './ui/textarea'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 const MessageForm = () => {
   return (
-    <div>MessageForm</div>
+    <div className=' mx-auto border bg-white space-y-4 rounded-lg p-6'>
+        <h2 className='text-2xl font-bold text-gray-800'>Dm Delay Button</h2>
+
+        <Textarea placeholder='Type'/>
+        <Input placeholder='Delay in secound' value={2}/>
+        <Button>send with delay</Button>
+    </div>
   )
 }
 
