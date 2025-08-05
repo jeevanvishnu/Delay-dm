3b4bf813c4fed524c62918a4389ab255f1bc1f26
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-08-05 17:12:53 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center max-h-svw mt-[20%]'>
+     <Home/>
+    </div>
+  )
+}
+
+export default App
